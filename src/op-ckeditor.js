bb3d0bc277e40445cedb4a4feea02f0a0005b232
zima <1216912253@qq.com>
2024-11-14 23:24:51 +0800

    调整ckeditor工具栏数量和顺序

--- src/op-ckeditor.js
+++ src/op-ckeditor.js
@@ -18,43 +18,55 @@ FullEditor.createCustomized = configurationCustomizer(FullEditor);
 FullEditor.builtinPlugins = builtinPlugins;
 FullEditor.defaultConfig = Object.assign({}, defaultConfig);
 FullEditor.defaultConfig.toolbar = {
-		items: [
-			'paragraph', 'heading1', 'heading2', 'heading3',
-			'|',
-			'bold',
-			'italic',
-			'strikethrough',
-			'code',
-			'insertCodeBlock',
-			'link',
-			'bulletedList',
-			'numberedList',
-			'todoList',
-			'imageUpload',
-			'blockQuote',
-			'|',
-			'insertTable',
-			'macroList',
-			'|',
-			'opContentRevisions',
-			'undo',
-			'redo',
-			'openProjectShowFormattingHelp',
-			'|',
-			'pageBreak',
-			'|',
-			'preview',
-			'opShowSource',
-			'|',
-			'fontSize',
-			// 'fontFamily',
-			'fontColor',
-			'fontBackgroundColor',
-			'horizontalLine',
-			'outdent', // 减少缩进
-			'indent', // 增加缩进
-			'highlight',
-		]
+	items: [
+		// 'heading', // 标题
+		'paragraph', 'heading1', 'heading2', 'heading3',
+		'|',
+
+		'imageUpload', // 图片上传
+		'fontSize', // 字体大小
+		// 'fontFamily',
+		'fontColor', // 字体颜色
+		'fontBackgroundColor', // 字体背景色
+		'highlight', // 高亮
+
+		'|',
+
+		'bold', // 加粗
+		'italic', // 斜体
+		'strikethrough', // 删除线
+		'link', // 超链接
+
+		'|',
+
+		'bulletedList', // 无序列表
+		'numberedList', // 有序列表
+		'todoList', // 代办列表
+		'outdent', // 减少缩进
+		'indent', // 增加缩进
+
+		'|',
+
+		'horizontalLine', // 水平线
+		'insertTable', // 表格
+		// 'macroList',
+
+		'|',
+
+		'code', // 代码
+		'insertCodeBlock', // 代码片段
+		'blockQuote', // 块引用
+
+		'|',
+
+		'opContentRevisions', // 显示本地修改
+		'undo', // 撤销
+		'redo', // 重做
+
+		'|',
+		// 'preview',
+		// 'opShowSource',
+	]
 };
 
 ConstrainedEditor.createCustomized = configurationCustomizer(ConstrainedEditor);
