4b1bf1c84e98721ac4a3717d40c48104f1514a1f
zima <1216912253@qq.com>
2024-11-14 22:03:30 +0800

    注释掉markdown处理器，使用默认的 HTML 数据处理器

--- src/commonmark/commonmark.js
+++ src/commonmark/commonmark.js
@@ -7,6 +7,7 @@ import CommonMarkDataProcessor from './commonmarkdataprocessor';
 
 // Simple plugin which loads the data processor.
 export default function CommonMarkPlugin(editor) {
-	editor.data.processor = new CommonMarkDataProcessor(editor.editing.view.document);
+	// 注释掉markdown处理器，使用默认的 HTML 数据处理器
+	// editor.data.processor = new CommonMarkDataProcessor(editor.editing.view.document);
 }
 
