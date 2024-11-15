98a15cf1050b695d4a624cfc46784a311ad1bff2
zima <1216912253@qq.com>
2024-11-15 23:36:56 +0800

    调整字体颜色插件的下拉框高度，貌似没起作用

--- src/op-plugins.js
+++ src/op-plugins.js
@@ -51,6 +51,8 @@ import { HeadingButtonsUI } from '@ckeditor/ckeditor5-heading';
 import { ParagraphButtonUI } from '@ckeditor/ckeditor5-paragraph';
 import { Highlight } from '@ckeditor/ckeditor5-highlight';
 
+import './styles.css';
+
 
 // We divide our plugins into separate concerns here
 // in order to enable / disable each group by configuration
